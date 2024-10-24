a7be68a72760103a4748ee217ff55ead3c18f9f9
Franco Piemontesi <franco219011@gmail.com>
2024-10-24 17:36:21 -0300

    include bootstrap js script add sample title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterModule, RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'tpi-dabd-integration-app';
+  title = 'AppName';
 }
