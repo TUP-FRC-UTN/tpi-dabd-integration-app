38da1e268e524ed814197730d7d91507cbfef1a3
405193-OLIVA <405193@tecnicatura.frc.utn.edu.ar>
2024-11-04 17:54:50 -0300

    export pdf excel

--- src/app/users/components/accounts/account-account-concept/account-account-concept.component.ts
+++ src/app/users/components/accounts/account-account-concept/account-account-concept.component.ts
@@ -1,18 +1,19 @@
-import { Component, inject } from '@angular/core';
+import { Component, ElementRef, inject, ViewChild } from '@angular/core';
 import { AccountService } from '../../../services/account.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AccountingConcept, ConceptTypes } from '../../../models/account';
 import { CommonModule, CurrencyPipe, Location } from '@angular/common';
-import { MainContainerComponent } from 'ngx-dabd-grupo01';
+import { Filter, FilterConfigBuilder, MainContainerComponent, TableFiltersComponent } from 'ngx-dabd-grupo01';
 import { PlotService } from '../../../services/plot.service';
 import { Plot } from '../../../models/plot';
 import { NgbPagination } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
+import { CadastreExcelService } from '../../../services/cadastre-excel.service';
 
 @Component({
   selector: 'app-account-account-concept',
   standalone: true,
-  imports: [CommonModule, MainContainerComponent, CurrencyPipe, NgbPagination, FormsModule],
+  imports: [CommonModule, MainContainerComponent, CurrencyPipe, NgbPagination, FormsModule, TableFiltersComponent],
   templateUrl: './account-account-concept.component.html',
   styleUrl: './account-account-concept.component.css'
 })
@@ -21,6 +22,9 @@ export class AccountAccountConceptComponent {
   private plotService = inject(PlotService)
   private activatedRoute = inject(ActivatedRoute)
   private router = inject(Router)
+  private excelService = inject(CadastreExcelService);
+
+  @ViewChild('conceptsTable') conceptsTable!: ElementRef;
 
   //#region ATT de PAGINADO
   currentPage: number = 0
@@ -29,11 +33,22 @@ export class AccountAccountConceptComponent {
   conceptList: AccountingConcept[] = [];
   lastPage: boolean | undefined
   totalItems: number = 0;
+  objectName : string = ""
   //#endregion
+
+  headers : string[] = ['Fecha', 'Concepto', 'Detalle', 'Monto']
+  LIMIT_32BITS_MAX = 2147483647;
+  dataMapper = (item: AccountingConcept) => [
+    item["accountingDate"],
+    this.translateDictionary(item["concept"], this.dictionaries[0]),
+    item["comments"],
+    item['amount'],
+  ];
 
   plotId!: number;
   plot!: Plot;
   conceptTypesDictionary = ConceptTypes;
+  dictionaries: Array<{ [key: string]: any }> = [this.conceptTypesDictionary];
 
   ngOnInit() {
     this.plotId = Number(this.activatedRoute.snapshot.paramMap.get('plotId'));
@@ -96,6 +111,17 @@ export class AccountAccountConceptComponent {
     return;
   }
 
+  translateDictionary(value: any, dictionary?: { [key: string]: any }) {
+    if (value !== undefined && value !== null && dictionary) {
+      for (const key in dictionary) {
+        if (dictionary[key].toString().toLowerCase() === value.toLowerCase()) {
+          return key;
+        }
+      }
+    }
+    return;
+  }
+
   //#region FUNCIONES PARA PAGINADO
   onItemsPerPageChange() {
     this.currentPage = 1;
@@ -107,4 +133,20 @@ export class AccountAccountConceptComponent {
     this.getAllConcepts(this.plotId);
   }
   //#endregion
+
+  exportToPdf() {
+    const table: HTMLTableElement = this.conceptsTable.nativeElement;
+    this.accountService.exportTableToPdf(table, `${this.getActualDayFormat()}_${this.objectName}`);
+  }
+
+  exportToExcel() {
+    const table: HTMLTableElement = this.conceptsTable.nativeElement;
+    this.accountService.exportTableToExcel(table, `${this.getActualDayFormat()}_${this.objectName}`);
+  }
+
+  getActualDayFormat() {
+    const today = new Date();
+
+    return today.toISOString().split('T')[0];
+  }
 }
--- src/app/users/services/account.service.ts
+++ src/app/users/services/account.service.ts
@@ -4,6 +4,10 @@ import { AccountingConcept } from '../models/account';
 import { map, Observable } from 'rxjs';
 import { PaginatedResponse } from '../models/api-response';
 import { AccountingConceptMapperPipe } from '../pipes/accounting-concept-mapper.pipe';
+import * as XLSX from 'xlsx';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
+import autoTable from 'jspdf-autotable';
 
 @Injectable({
   providedIn: 'root'
@@ -28,5 +32,41 @@ export class AccountService {
         }
       })
     );
+  }
+
+  exportTableToPdf(table: HTMLTableElement, pdfFileName: string): void {
+    html2canvas(table).then((canvas: any) => {
+      const imgData = canvas.toDataURL('image/png');
+      const pdf = new jsPDF('p', 'mm', 'a4');
+      const pdfWidth = pdf.internal.pageSize.getWidth();
+      const pdfHeight = pdf.internal.pageSize.getHeight();
+      const imgWidth = pdfWidth - 20; // Ajuste para los márgenes
+      const imgHeight = (canvas.height * imgWidth) / canvas.width;
+      // const imgWidth = 190;
+      // const imgHeight = (canvas.height * imgWidth) / canvas.width;
+      // pdf.addImage(imgData, 'PNG', 10, 10, imgWidth, imgHeight);
+      if (imgHeight < pdfHeight) {
+        pdf.addImage(imgData, 'PNG', 10, 10, imgWidth, imgHeight);
+      } else {
+        // Si es más grande, dividir en varias páginas
+        let position = 10;
+        while (position < imgHeight) {
+          pdf.addImage(imgData, 'PNG', 10, position, imgWidth, imgHeight);
+          position -= pdfHeight;
+          if (position < imgHeight - pdfHeight) pdf.addPage();
+        }
+      }
+      pdf.save(`${pdfFileName}.pdf`);
+    });
+  }
+
+  exportTableToExcel(table: HTMLTableElement, excelFileName: string): void {
+    const clonedTable = table.cloneNode(true) as HTMLTableElement;
+    
+    const worksheet: XLSX.WorkSheet = XLSX.utils.table_to_sheet(clonedTable);
+    const workbook: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
+
+    XLSX.writeFile(workbook, `${excelFileName}.xlsx`);
   }
 }
