a6e38a325d479f50924842dbb2af03623142d0ce
113207-BURASCHI-MATEO <113207@tecnicatura.frc.utn.edu.com.ar>
2024-11-10 18:59:23 -0300

    file

--- src/app/users/services/file.service.ts
+++ src/app/users/services/file.service.ts
@@ -1,25 +1,17 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
-<<<<<<< HEAD
 import { environment } from '../../../environments/environment';
-=======
 import { SessionService } from './session.service';
->>>>>>> develop
 
 @Injectable({
   providedIn: 'root',
 })
 export class FileService {
-<<<<<<< HEAD
   private apiUrl: string = environment.production
     ? `${environment.apis.cadastre}files`
     : 'http://localhost:8004/files';
-=======
   private sessionService = inject(SessionService);
-
-  private apiUrl = 'http://localhost:8004/files';
->>>>>>> develop
 
   constructor(private http: HttpClient) {}
 
@@ -31,17 +23,8 @@ export class FileService {
     );
   }
 
-<<<<<<< HEAD
-  updateFileStatus(
-    fileId: number,
-    status: any,
-    note: any,
-    userId: string
-  ): Observable<any> {
-=======
 
   updateFileStatus(fileId: number, status: any, note: any): Observable<any> {
->>>>>>> develop
     const headers = new HttpHeaders({
       'x-user-id': this.sessionService.getItem('user').id.toString(),
     });
