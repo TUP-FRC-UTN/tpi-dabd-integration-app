ad2c5c1d76eb2e17e72e2ead0ea719fa420ce319
RamiroRomera <405343@tecnicatura.frc.utn.edu.ar>
2024-11-13 02:26:44 -0300

    Commons

---
+++ src/app/users/components/commons/kpi/kpi.component.ts
@@ -0,0 +1,13 @@
+import {Component, Input} from '@angular/core';
+import {kpiModel} from "../../../../models/dashboard.model";
+
+@Component({
+  selector: 'app-kpi',
+  standalone: true,
+  imports: [],
+  templateUrl: './kpi.component.html',
+  styleUrl: './kpi.component.css'
+})
+export class KpiComponent {
+  @Input() kpi:kpiModel = {} as kpiModel
+}
