d8ce65e12115ccf17c7bba225286b73435445032
2W1-113114-Rivera Ignacio <113114@tecnicatura.frc.utn.edu.ar>
2024-11-13 00:45:09 -0300

    Errores ortográficos 2

--- src/app/users/components/users/users-created-by-user/users-created-by-user.component.ts
+++ src/app/users/components/users/users-created-by-user/users-created-by-user.component.ts
@@ -77,7 +77,7 @@ export class UsersCreatedByUserComponent {
         }
       })
     } else {
-      this.toastService.sendError("Error al cargar la página, reintente.")
+      this.toastService.sendError("Error al cargar la pagina, reintente")
     }
   }
 
