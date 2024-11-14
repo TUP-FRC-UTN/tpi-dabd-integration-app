d71e052621b8d9aacede30d6673c5de178197df1
Scipioni-Gabriel-405262 <405262@tecnicatura.frc.utn.edu.ar>
2024-11-13 23:23:23 -0300

    lista la const

--- src/app/expenses/const.ts
+++ src/app/expenses/const.ts
@@ -1,1 +1,3 @@
-export const PORT = "http://localhost:8088/"
+import { environment } from "../../environments/environment"
+
+export const PORT = environment.production ? `${environment.apis.expenses}` : `${environment.apis.expenses}`
