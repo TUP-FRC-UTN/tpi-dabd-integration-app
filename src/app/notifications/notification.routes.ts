13e5f86e68d69758864402b46dfcf7041e55e288
113809-Aguirre-Salvador <113809@tecnicatura.frc.utn.edu.ar>
2024-11-14 13:27:02 -0300

    Comments TODO REMOVE

--- src/app/notifications/notification.routes.ts
+++ src/app/notifications/notification.routes.ts
@@ -18,7 +18,7 @@ export const NOTIFICATION_ROUTES: Routes = [
   {
     path: 'templates/new',
     component: TemplateEmailComponent,
-    canActivate: [authGuard],
+    //canActivate: [authGuard],
     canMatch: [hasRoleCodeGuard],
     data: {
       allowedRoleCodes: [
@@ -31,7 +31,7 @@ export const NOTIFICATION_ROUTES: Routes = [
   {
     path: 'templates',
     component: TemplateListComponent,
-    canActivate: [authGuard],
+    //canActivate: [authGuard],
     canMatch: [hasRoleCodeGuard],
     data: {
       allowedRoleCodes: [
@@ -145,8 +145,8 @@ export const NOTIFICATION_ROUTES: Routes = [
   {
     path: 'notification/charts',
     component: NotificationChartComponent,
-    canActivate: [authGuard],
-    canMatch: [hasRoleCodeGuard],
+    //canActivate: [authGuard],
+    //canMatch: [hasRoleCodeGuard],
     data: {
       allowedRoleCodes: [
         URLTargetType.SUPERADMIN,
