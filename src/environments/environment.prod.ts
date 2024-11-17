393c2aa5c0f94ef72d96825bb2af44b9a4bd27f4
404975-Zabala-joaquin <404975@tecnicatura.frc.utn.edu.ar>
2024-11-17 00:38:03 -0300

    change url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 
 // environment.prod.ts producción con nginx segun backend 
-const API_URL = 'https://lbsm4xgt-8080.brs.devtunnels.ms/';
+const API_URL = 'http://localhost:8080/';
 
 export const environment = {
   production: true,
